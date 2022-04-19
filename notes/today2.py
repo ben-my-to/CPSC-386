146f662e8e1e80166b0c5afe9aa7307a7397c341
/home/jason/.gitconfig <reddkingdom@csu.fullerton.edu>
2022-04-19 12:46:02 -0700

    new

--- notes/today2.py
+++ notes/today2.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
+
+import pygame
 from math import isclose
 
-print(isclose(0.2*3, 0.6))
+
+class Circle:
+    def __init__(self, x, y, radius):
+        self._center = pygame.Vector2(x, y)
+        self._radius = radius
+
+    @property
+    def center(self):
+        return self._center
+
+    @property
+    def radius(self):
+        return self._radius
+
+    def has_collide(self, other_circle):
+        v = self.center - other_circle.center
+        return len(d) <= self.radius + other_circle.radius
+
+
+
+print(isclose(0.2 * 3, 0.6))
 
 
 
